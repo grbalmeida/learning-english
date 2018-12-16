afbb4e1fc8e7a3889f7615e927e9e0aa5e6c49f1
gilvan <g.r.almeida@live.com>
2018-12-16 19:00:46 -0200

    Displaying the current phrase dynamically

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core'
+import {Phrase} from '../shared/phrase.model'
+import PHRASES from './phrases-mock'
 
 @Component({
   selector: 'app-panel',
@@ -7,9 +9,48 @@ import {Component, OnInit} from '@angular/core'
 })
 export class PanelComponent implements OnInit {
 
-  constructor() { }
+  public instruction: string = 'Traduza a frase:'
+  public phrases: Array<Phrase> = PHRASES
+  public currentIndex: number
+  public ratios: Array<number>
+  public currentPhrase: Phrase
+  public response: string
+
+  constructor() { 
+    this.ratios = []
+    this.setCurrentIndex()
+    this.setCurrentPhrase()
+  }
 
   ngOnInit() {
   }
 
+  public getRandomIndex(): number {
+    const min = 0
+    const max = this.phrases.length - 1
+    const newIndex: number = Math.floor(Math.random() * max) + min
+
+    if(!this.ratios.includes(newIndex)) {
+      this.ratios.push(newIndex)
+      return newIndex
+    }
+
+    this.getRandomIndex()
+  }
+
+  public setCurrentPhrase(): void {
+    this.currentPhrase = this.phrases[this.currentIndex]
+  }
+
+  public setCurrentIndex(): void {
+    this.currentIndex = this.getRandomIndex()
+  }
+
+  public updateResponse(response: Event): void {
+    this.response = ((<HTMLInputElement>response.target)).value
+  } 
+
+  public verifyResponse(): void {
+
+  }
 }

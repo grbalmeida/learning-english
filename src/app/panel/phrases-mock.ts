fae347f8cbc71f69ffda795b1fdf36a107a55dba
gilvan <g.r.almeida@live.com>
2018-12-16 18:07:25 -0200

    Creating a Mock of Phrase Objects

---
+++ src/app/panel/phrases-mock.ts
@@ -0,0 +1,25 @@
+import {Phrase} from '../shared/phrase.model'
+
+const PHRASES: Array<Phrase> = [
+    {englishPhrase: 'I like to learn', portuguesePhrase: 'Eu gosto de aprender'},
+    {englishPhrase: 'Good morning', portuguesePhrase: 'Bom dia'},
+    {englishPhrase: 'What a nice day', portuguesePhrase: 'Que dia bonito'},
+    {englishPhrase: 'Are you awake?', portuguesePhrase: 'Você está acordado?'},
+    {englishPhrase: 'What time is it?', portuguesePhrase: 'Que horas são?'},
+    {englishPhrase: 'What day is today?', portuguesePhrase: 'Que dia é hoje?'},
+    {englishPhrase: 'I\'m feeling sick today', portuguesePhrase: 'Eu estou me sentindo doente hoje'},
+    {englishPhrase: 'I\'m running a little late', portuguesePhrase: 'Eu estou um pouco atrasado'},
+    {englishPhrase: 'How was school today?', portuguesePhrase: 'Como foi a escola hoje?'},
+    {englishPhrase: 'I took an exam and did well', portuguesePhrase: 'Eu fiz uma prova e me saí bem'},
+    {englishPhrase: 'I\'m hungry', portuguesePhrase: 'Estou com fome'},
+    {englishPhrase: 'Is lunch ready?', portuguesePhrase: 'O almoço está pronto?'},
+    {englishPhrase: 'Do you want some water?', portuguesePhrase: 'Você quer um pouco de água?'},
+    {englishPhrase: 'It\'s too hot today!', portuguesePhrase: 'Está muito quente hoje!'},
+    {englishPhrase: 'I have to go to the supermarket', portuguesePhrase: 'Eu tenho que ir ao supermercado'},
+    {englishPhrase: 'Do you want to watch a movie?', portuguesePhrase: 'Você quer ver um filme?'},
+    {englishPhrase: 'Is there anything good on TV?', portuguesePhrase: 'Tem alguma coisa boa na TV?'},
+    {englishPhrase: 'I\'m going to bed', portuguesePhrase: 'Eu vou para a cama'},
+    {englishPhrase: 'Good night. Sweet dreams.', portuguesePhrase: 'Boa noite. Bons sonhos.'}
+]
+
+export default PHRASES

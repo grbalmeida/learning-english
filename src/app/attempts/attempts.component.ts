1c754f3601cf5eb3ad536a141709de3b97fe63ba
gilvan <g.r.almeida@live.com>
2018-12-18 00:01:10 -0200

    Creating the Heart class

--- src/app/attempts/attempts.component.ts
+++ src/app/attempts/attempts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Heart} from '../shared/heart.model'
 
 @Component({
   selector: 'app-attempts',
@@ -9,6 +10,11 @@ export class AttemptsComponent implements OnInit {
 
   public emptyHeart: string = '../../assets/empty-heart.png'
   public fullHeart: string = '../../assets/full-heart.png'
+  public hearts: Array<Heart> = [
+    new Heart(true),
+    new Heart(true),
+    new Heart(true)
+  ]
 
   constructor() { }
 
---
+++ src/app/shared/heart.model.ts
@@ -0,0 +1,14 @@
+export class Heart {
+    constructor(
+        public full: boolean, 
+        public fullHeartUrl: string = '../../assets/full-heart.png',
+        public emptyHeartUrl: string = '../../assets/empty-heart.png'
+    ) {}
+
+    public displaysHeart(): string {
+        if(this.full) {
+            return this.fullHeartUrl
+        }
+        return this.emptyHeartUrl
+    }
+}
